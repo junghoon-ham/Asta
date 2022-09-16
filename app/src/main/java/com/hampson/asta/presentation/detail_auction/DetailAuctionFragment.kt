ed81e9f4f063bd6a2e80f42ac450cb7ecdc071b8
junghoon.ham <junghoon.ham@influential.co.kr>
2022-09-16 18:04:31 +0900

    디테일 작업중

--- app/src/main/java/com/hampson/asta/presentation/detail_auction/DetailAuctionFragment.kt
+++ app/src/main/java/com/hampson/asta/presentation/detail_auction/DetailAuctionFragment.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
+import com.hampson.asta.R
 import com.hampson.asta.databinding.FragmentDetailAuctionBinding
+import com.hampson.asta.domain.model.Product
 import com.hampson.asta.presentation.BaseFragment
+import com.hampson.asta.presentation.home.HomeFragmentDirections
+import com.hampson.asta.presentation.home.TestAdapter
 
 class DetailAuctionFragment : BaseFragment() {
     private var _binding: FragmentDetailAuctionBinding? = null
@@ -18,6 +27,27 @@ class DetailAuctionFragment : BaseFragment() {
     ): View? {
         _binding = FragmentDetailAuctionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>()
+
+        imageList.add(SlideModel(R.drawable.test_img1, null, ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel(R.drawable.test_img2, null, ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel(R.drawable.test_img3, null, ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel(R.drawable.test_img4, null, ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel(R.drawable.test_img6, null, ScaleTypes.CENTER_CROP))
+
+        binding.imageSlider.setImageList(imageList)
+
+        binding.toolbar.apply {
+            setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+            setNavigationOnClickListener {
+                requireActivity().onBackPressed()
+            }
+        }
     }
 
     override fun onDestroyView() {
